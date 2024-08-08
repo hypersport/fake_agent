b07752cd6abdd0a774f5c00a3312728e982e1a62
hypersport <boss.yuan@outlook.com>
2024-08-08 10:05:43 +0000

    feat: add new generation options

--- lib/fake_agent.rb
+++ lib/fake_agent.rb
@@ -5,73 +5,77 @@ require "json"
 
 module FakeAgent
   class FakeAgent
-    @browsers = %w[edge chrome safari firefox]
+    @browsers = %w[edge chrome safari firefox duckduckgo]
     @os = %w[windows macos linux android ios]
     @platforms = %w[pc mobile tablet]
     agents = File.read(File.join(__dir__, "browsers.json"))
     @data = JSON.parse(agents)
 
     class << self
-      def random
-        get_agent(@data)
+      def random(browsers: @browsers, systems: @os, platforms: @platforms)
+        get_agent(browsers: Array(browsers), systems: Array(systems), platforms: Array(platforms))
       end
 
-      def chrome
-        get_agent(@data.select { |x| x["browser"] == "chrome" })
+      def chrome(systems: @os, platforms: @platforms)
+        get_agent(browsers: %w[chrome], systems: Array(systems), platforms: Array(platforms))
       end
 
-      def safari
-        get_agent(@data.select { |x| x["browser"] == "safari" })
+      def safari(systems: @os, platforms: @platforms)
+        get_agent(browsers: %w[safari], systems: Array(systems), platforms: Array(platforms))
       end
 
-      def firefox
-        get_agent(@data.select { |x| x["browser"] == "firefox" })
+      def firefox(systems: @os, platforms: @platforms)
+        get_agent(browsers: %w[firefox], systems: Array(systems), platforms: Array(platforms))
       end
 
-      def edge
-        get_agent(@data.select { |x| x["browser"] == "edge" })
+      def edge(systems: @os, platforms: @platforms)
+        get_agent(browsers: %w[edge], systems: Array(systems), platforms: Array(platforms))
       end
 
-      def duckduckgo
-        get_agent(@data.select { |x| x["browser"].downcase.include? "duckduckgo" })
+      def duckduckgo(systems: @os, platforms: @platforms)
+        get_agent(browsers: %w[duckduckgo], systems: Array(systems), platforms: Array(platforms))
       end
 
-      def windows
-        get_agent(@data.select { |x| x["os"] == "win10" || x["os"] == "win11" })
+      def windows(browsers: @browsers, platforms: @platforms)
+        get_agent(browsers: Array(browsers), systems: %w[windows], platforms: Array(platforms))
       end
 
-      def macos
-        get_agent(@data.select { |x| x["os"] == "macos" })
+      def macos(browsers: @browsers, platforms: @platforms)
+        get_agent(browsers: Array(browsers), systems: %w[macos], platforms: Array(platforms))
       end
 
-      def linux
-        get_agent(@data.select { |x| x["os"] == "linux" })
+      def linux(browsers: @browsers, platforms: @platforms)
+        get_agent(browsers: Array(browsers), systems: %w[linux], platforms: Array(platforms))
       end
 
-      def android
-        get_agent(@data.select { |x| x["os"] == "android" })
+      def android(browsers: @browsers, platforms: @platforms)
+        get_agent(browsers: Array(browsers), systems: %w[android], platforms: Array(platforms))
       end
 
-      def ios
-        get_agent(@data.select { |x| x["os"] == "ios" })
+      def ios(browsers: @browsers, platforms: @platforms)
+        get_agent(browsers: Array(browsers), systems: %w[ios], platforms: Array(platforms))
       end
 
-      def pc
-        get_agent(@data.select { |x| x["type"] == "pc" })
+      def pc(browsers: @browsers, systems: @os)
+        get_agent(browsers: Array(browsers), systems: Array(systems), platforms: %w[pc])
       end
 
-      def mobile
-        get_agent(@data.select { |x| x["type"] == "mobile" })
+      def mobile(browsers: @browsers, systems: @os)
+        get_agent(browsers: Array(browsers), systems: Array(systems), platforms: %w[mobile])
       end
 
-      def tablet
-        get_agent(@data.select { |x| x["type"] == "tablet" })
+      def tablet(browsers: @browsers, systems: @os)
+        get_agent(browsers: Array(browsers), systems: Array(systems), platforms: %w[tablet])
       end
 
       private
 
-      def get_agent(browsers)
-        browsers[Random.rand(browsers.length)]["useragent"]
+      def get_agent(browsers: @browsers, systems: @os, platforms: @platforms)
+        data = @data.select do |x|
+          browsers.include? x["browser"] and systems.include? x["os"] and platforms.include? x["platform"]
+        end
+        data = @data if data.length <= 0
+        data[Random.rand(data.length)]["useragent"]
       end
     end
   end
